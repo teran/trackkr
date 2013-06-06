40a4c249666160f7abbb44ada0a110c62b798211
Igor Shishkin <me@teran.ru>
2013-06-06 14:52:34 +0400

    core and api apps

---
+++ core/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Unit(models.Model):
+    name = models.CharField(max_length=128)
+    description = models.TextField(blank=True)
+    imei = models.CharField(max_length=128)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+    user = models.ManyToManyField(User, related_name='units')
+
+    def __unicode__(self):
+        return '%s (%s)' % (self.name, self.imei)
+
+    def get_last_pos(self):
+        pass
+
+
+class Message(models.Model):
+    MESSAGE_TYPES = (
+        (1, 'logon'),
+        (2, 'heartbeat'),
+        (3, 'low_battery'),
+        (4, 'sos'),
+        (5, 'tracker'))
+
+    timestamp = models.DateTimeField(auto_now_add=True)
+    unit = models.ForeignKey(Unit, related_name='messages')
+    longitude = models.FloatField(blank=True)
+    latitude = models.FloatField(blank=True)
+    message_type = models.IntegerField(max_length=2,
+                                       choices=MESSAGE_TYPES)
+
+    def __unicode__(self):
+        return '%s, %s (%s)' % (self.longitude, self.latitude, self.timestamp)
