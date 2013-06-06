263299f12fe860318d48969388a93b6b920003d7
Igor Shishkin <me@teran.ru>
2013-06-06 14:50:23 +0400

    sqlite db added for development purposes

--- trackkr/settings.py
+++ trackkr/settings.py
@@ -16,7 +16,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'NAME': '%s/db/development.sqlite3' % PROJECT_ROOT,  # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
