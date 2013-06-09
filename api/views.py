11b125038292954a5875153d0c8205602da69c9f
Igor Shishkin <me@teran.ru>
2013-06-09 22:59:59 +0400

    api hands implemented to access through json

--- api/views.py
+++ api/views.py
@@ -1,1 +1,27 @@
-# Create your views here.
+import json
+
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+from django.shortcuts import get_object_or_404, redirect, HttpResponse, Http404
+from django.template import RequestContext
+
+from core.models import Unit
+
+
+@login_required
+def lastpos(request, imei):
+    unit = get_object_or_404(Unit, imei=imei)
+    lastloc = unit.messages.filter(~Q(latitude=None, longitude=None)).order_by('-timestamp')[0]
+
+    return HttpResponse(content=json.dumps({'latitude': lastloc.latitude,
+                                            'longitude': lastloc.longitude,
+                                            'timestamp': lastloc.timestamp.strftime('%Y-%m-%dT%H:%M:%S')}),
+                        content_type='application/json')
+
+
+@login_required
+def units(request):
+    units = Unit.objects.filter(user=request.user)
+
+    return HttpResponse(content=json.dumps([{'imei':unit.imei, 'name':unit.name} for unit in units]),
+                        content_type='application/json')
--- trackkr/urls.py
+++ trackkr/urls.py
@@ -6,6 +6,8 @@ urlpatterns = patterns(
     (r'^dashboard.html$', 'webui.views.dashboard'),
     (r'^login.html$', 'webui.views.log_in'),
     (r'^logout.html$', 'webui.views.log_out'),
+    (r'^api/units/(?P<imei>[0-9]+).json$', 'api.views.lastpos'),
+    (r'^api/units.json', 'api.views.units'),
     (r'^units.html$', 'webui.views.units'),
     (r'^units/add.html$', 'webui.views.unit_add'),
     (r'^units/delete/(?P<imei>[0-9a-z]+).html$', 'webui.views.unit_delete'),
