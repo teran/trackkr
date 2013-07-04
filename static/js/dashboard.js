d99f96db7c4fd4c3a3300eb749d9766552ab4152
Igor Shishkin <me@teran.ru>
2013-07-04 11:47:16 +0400

    moving really big part of javascript to separate file .js

---
+++ static/js/dashboard.js
@@ -0,0 +1,42 @@
+$('document').ready(function(){
+    ymaps.ready(function(){
+        $.getJSON('/api/recentpos.json')
+            .done(function(loc){
+                var recentpos = [loc.latitude, loc.longitude];
+                var dashboard_map = new ymaps.Map("dashboard-map", {
+                    center: recentpos,
+                    zoom: 10
+                    });
+                
+                dashboard_map.controls
+                    .add('zoomControl', { left: 5, top: 5 })
+                    .add('typeSelector')
+                    .add('mapTools', { left: 35, top: 5 });
+    
+                $.getJSON('/api/units.json')
+                    .done(function(data){
+                        $.each(data, function(element, object) {
+                            $.getJSON('/api/units/'+object.imei+'.json')
+                                .done(function(loc){
+                                    dashboard_map.geoObjects.add(new ymaps.Placemark(
+                                            [loc.latitude, loc.longitude],
+                                            {
+                                                balloonContentHeader: loc.name,
+                                                balloonContentBody: loc.timestamp
+                                            }
+                                    ));
+                                })
+                                .fail(function(jqxhr, textStatus, error){
+                                    alert('fail: '+jqxhr+textStatus+error);
+                                });
+                        });
+            })
+            .fail(function(jqxhr, textStatus, error){
+                alert('fail: '+jqxhr+textStatus+error);
+            });
+            })
+            .fail(function() {
+                alert('Error retrieving latest unit position');
+            });
+        });
+});
