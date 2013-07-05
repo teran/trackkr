8969c2c7b1a670197c9520ffbefbe0c3dc0fb771
Igor Shishkin <me@teran.ru>
2013-07-05 12:42:30 +0400

    history mode partially worked

--- static/js/unit.js
+++ static/js/unit.js
@@ -1,10 +1,11 @@
 $('document').ready(function(){
     ymaps.ready(function(){
         var imei = $('.unit-imei').text();
+        var unit_map;
         $.getJSON('/api/units/'+imei+'.json')
             .done(function(data){
                 var recentpos = [data.latitude, data.longitude];
-                var unit_map = new ymaps.Map("unit-map", {
+                unit_map = new ymaps.Map("unit-map", {
                         center: recentpos,
                         zoom: 10
                     });
@@ -33,7 +34,26 @@ $('document').ready(function(){
             
             $.getJSON('/api/units/location/'+pk+'.json')
                 .done(function(data){
-                    alert('latitude: '+data.latitude+' ; longitude: '+data.longitude);
+                    unit_map.destroy();
+                    unit_map = new ymaps.Map("unit-map", {
+                        center: [data.latitude, data.longitude],
+                        zoom: 10
+                    });
+                
+                    unit_map.controls
+                        .add('zoomControl', { left: 5, top: 5 })
+                        .add('typeSelector')
+                        .add('mapTools', { left: 35, top: 5 });
+                        
+                    unit_map.geoObjects.add(
+                        new ymaps.Placemark(
+                            [data.latitude, data.longitude],
+                            {
+                                balloonContentHeader: data.name,
+                                balloonContentBody: data.timestamp
+                            }
+                        ));
+                    $(this).parent().addClass('active');
                 })
                 .fail(function(){
                     
