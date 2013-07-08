f7f361d424cd3067662e4e8afb9a4702afb71197
Igor Shishkin <me@teran.ru>
2013-07-08 12:29:43 +0400

    cleanup from unused views https://github.com/teran/trackkr/issues/3

--- api/views.py
+++ api/views.py
@@ -11,30 +11,6 @@ from core.models import Unit, Message
 
 
 @login_required
-def lastpos(request, imei):
-    unit = get_object_or_404(Unit, imei=imei)
-    lastloc = unit.get_lastlocation()
-
-    try:
-        out = {
-            'latitude': lastloc.latitude,
-            'longitude': lastloc.longitude,
-            'name': unit.name,
-            'timestamp': unit.lastseen()
-        }
-    except:
-        out = {
-            'latitude': None,
-            'longitude': None,
-            'name': unit.name,
-            'timestamp': None
-        }
-
-    return HttpResponse(content=json.dumps(out),
-                        content_type='application/json')
-
-
-@login_required
 def location(request):
     try:
         imei = int(request.GET['imei'])
@@ -83,21 +59,3 @@ def location(request):
                 'name': unit.name,
                 'timestamp': None
             }), content_type='application/json')
-
-
-@login_required
-def units(request):
-    units = Unit.objects.filter(user=request.user)
-
-    return HttpResponse(content=json.dumps([{'imei': unit.imei, 'name': unit.name} for unit in units]),
-                        content_type='application/json')
-
-
-@login_required
-def recentpos(request):
-    unit = Unit.objects.filter(user=request.user)
-    lastloc = Message.objects.filter(~Q(latitude=None, longitude=None), unit__in=unit).order_by('-timestamp')[0]
-
-    return HttpResponse(content=json.dumps({'latitude': lastloc.latitude,
-                                            'longitude': lastloc.longitude}),
-                        content_type='application/json')
