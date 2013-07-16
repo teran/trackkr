5d6fbbc432334c12f0023dabe0d22f7848a03fc9
Igor Shishkin <me@teran.ru>
2013-07-16 13:03:10 +0400

    Checkin if got right location from location.json and rendering geoobject or empty map in dependence(Fixes #8)

--- static/js/unit.js
+++ static/js/unit.js
@@ -4,24 +4,31 @@ $('document').ready(function(){
         var unit_map;
         $.getJSON('/api/location.json?imei='+imei)
             .done(function(data){
-                var recentpos = [data.latitude, data.longitude];
-                unit_map = new ymaps.Map("unit-map", {
-                        center: recentpos,
-                        zoom: 10
-                    });
-
+                if(data.latitude && data.longitude) {
+                    var recentpos = [data.latitude, data.longitude];
+                    unit_map = new ymaps.Map("unit-map", {
+                            center: recentpos,
+                            zoom: 10
+                        });
+    
+                    unit_map.geoObjects.add(
+                        new ymaps.Placemark(
+                            [data.latitude, data.longitude], {
+                                balloonContentHeader: data.name,
+                                balloonContentBody: data.timestamp
+                            }
+                        ));
+                } else {
+                    unit_map = new ymaps.Map("unit-map", {
+                            center: [ 0, 0 ],
+                            zoom: 2
+                        });
+                }
+                
                 unit_map.controls
                     .add('zoomControl', { left: 5, top: 5 })
                     .add('typeSelector')
                     .add('mapTools', { left: 35, top: 5 });
-
-                unit_map.geoObjects.add(
-                    new ymaps.Placemark(
-                        [data.latitude, data.longitude], {
-                            balloonContentHeader: data.name,
-                            balloonContentBody: data.timestamp
-                        }
-                    ));
             })
             .fail(function(jqxhr, textStatus, error) {
                 $('.notifications')
@@ -42,7 +49,7 @@ $('document').ready(function(){
             } else {
                 url = '/api/location.json?imei='+imei+'&message='+pk;
             }
-    
+
             $.getJSON(url)
                 .done(function(data){
                     unit_map.destroy();
